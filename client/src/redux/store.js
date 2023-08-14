a0f939eef7f9c7f2ccd28690707f5143f920b699
Isha Kharkia <ishakharkia@gmail.com>
2023-08-14 22:57:16 +0530

    commit

---
+++ client/src/redux/store.js
@@ -0,0 +1,35 @@
+import cartReducer from "./cartReducer";
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+/*const stripe = require('stripe')('sk_test_51NSmZ8SHGFEAIfvcW12UuylTawx137Eew7Ia9IqUwdBqVJ94zNLIaE50fMH0YafeX1l0vj3t5c28lYMlTx913cSg00eeFyncry');*/
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, cartReducer);
+
+export const store = configureStore({
+  reducer: {
+    cart: persistedReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export let persistor = persistStore(store);
